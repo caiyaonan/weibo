ea86ae4cae7f37ff2abda6b92f28746d51c738ea
caiyaonan <caiyaonan@vip.qq.com>
2019-08-29 07:23:53 +0000

    基础页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('laouts.default')
+@section('title','帮助')
+@section('content')
+    <h1>帮助</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('laouts.default')
+@section('title','Weibo app')
+@section('content')
+    <h1>主页</h1>
+@stop
